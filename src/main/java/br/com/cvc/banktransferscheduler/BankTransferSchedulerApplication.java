f0e0deba093b6067b3fd08c0b12ba1e6c3fccb43
Victor Xavier <victorxm1@hotmail.com>
2021-04-09 21:33:58 -0300

    starting development

---
+++ src/main/java/br/com/cvc/banktransferscheduler/BankTransferSchedulerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvc.banktransferscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransferSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransferSchedulerApplication.class, args);
+	}
+
+}
