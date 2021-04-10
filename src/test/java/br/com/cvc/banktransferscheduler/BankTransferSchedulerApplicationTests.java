ae76fecfd56dab7a8041541fa924000da2b1913a
Victor Xavier <victorxm1@hotmail.com>
2021-04-09 21:35:43 -0300

    starting development

---
+++ src/test/java/br/com/cvc/banktransferscheduler/BankTransferSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cvc.banktransferscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
