9c8791a70cd0fd947139b0a0e3c2fb1ad43c50c5
Victor Xavier <victorxm1@hotmail.com>
2021-04-16 19:38:12 -0300

    adding swagger documentation

--- src/main/java/br/com/cvc/banktransferscheduler/BankTransferSchedulerApplication.java
+++ src/main/java/br/com/cvc/banktransferscheduler/BankTransferSchedulerApplication.java
@@ -4,17 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-
 @SpringBootApplication
 @EnableAutoConfiguration
 public class BankTransferSchedulerApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(BankTransferSchedulerApplication.class, args);
-	}
+        SpringApplication.run(BankTransferSchedulerApplication.class, args);
+    }
 
 }
